966cf747769e2025803217161eb7fe8b8b297cb0
Fabrice Sapienza <sapienza.fabrice@gmail.com>
2022-10-27 12:19:54 +0200

    fix error build

--- components/Sections/About/About.jsx
+++ components/Sections/About/About.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-scroll';
-import humanDev from '../../../public/assets/humanDev.gif'
+import { Link } from "react-scroll";
+import humanDev from "../../../public/assets/humanDev.gif";
 
 export default function About({ anchorScrollSpy }) {
 
     return (
         <section className="bg-custom text-secondary" id={anchorScrollSpy}>
-            <h1 className='text-center font-extrabold text-5xl mb-2 pt-28 md:text-6xl word-spacing'>
-                <span className='my-3'>Développeur</span>
+            <h1 className="text-center font-extrabold text-5xl mb-2 pt-28 md:text-6xl word-spacing">
+                <span className="my-3">Développeur</span>
                 <span className="my-3 ml-3 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-primary relative inline-block">
                     <span className="relative text-white">Front</span>
                 </span>
@@ -19,11 +19,11 @@ export default function About({ anchorScrollSpy }) {
                     >
                         <h3 className="text-3xl font-extrabold">À propos</h3>
                         <h4 className="text-xl font-bold my-3">Développeur Front-end React.js & intégrateur web</h4>
-                        <p className="mb-2">Passionné de développement web, j'ai effectué une reconversion professionnelle en 2016.</p>
-                        <p className="mb-2">Etant autodidact et très sensible à la qualité du code, l'ergonomie, la fluidité et l'expérience utilisateur (UX) d'un site web, je n'hésite pas à me former régulièrement.</p>
+                        <p className="mb-2">Passionné de développement web, j’ai effectué une reconversion professionnelle en 2016.</p>
+                        <p className="mb-2">Etant autodidact et très sensible à la qualité du code, l’ergonomie, la fluidité et l’expérience utilisateur (UX) d’un site web, je n’hésite pas à me former régulièrement.</p>
                         <p className="mb-2">je serais très heureux de discuter avec vous de vos projets ou de belles opportunités professionnelles.</p>
                         <p className="md-2">
-                            N'hésitez pas à me <Link to="contact" href="#contact" smooth={true} offset={-150} duration={500} className="cursor-pointer text-primary font-semibold">contacter</Link>.
+                            N’hésitez pas à me <Link to="contact" href="#contact" smooth={true} offset={-150} duration={500} className="cursor-pointer text-primary font-semibold">contacter</Link>.
                         </p>
                     </div>
                     <div>
