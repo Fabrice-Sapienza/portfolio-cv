afef67bd2a6785c9e282b909720044f95d5edef9
Fabrice Sapienza <sapienza.fabrice@gmail.com>
2022-10-30 17:42:35 +0100

    update CardAlert

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // env:{
-  //   EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
-  //   EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
-  //   EMAILJS_USER_ID: process.env.EMAILJS_USER_ID
-  // }
 }
 
 module.exports = nextConfig
